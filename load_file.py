0230b0f9837f58d45391425f28d1c98bdca471ee
Greg Smith <smith.8391@osu.edu>
2019-05-26 21:35:31 -0400

    changes to load_file.py - a little more memory-efficient - more careful handling of timezones - added load_all_files function to load several files at once - aded temp_save and temp_load for saving pd.df's to disk
    zip_codes.py:
    - converts zip codes to time zones and states
    - uses timezone info to change to local time
    - examples at the bottom of the file

--- load_file.py
+++ load_file.py
@@ -8,10 +8,8 @@ Created on Fri May 24 13:55:18 2019
 #%% import stuff
 import pandas as pd
 import numpy as np
-import time
-
-#import seaborn as sns
-#import matplotlib.pyplot as plt
+import time, os, glob
+import pyarrow.parquet as pq
 
 def mem_usage(pd_obj):
     '''calculates the memory footprint of a pandas object'''
@@ -22,28 +20,33 @@ def mem_usage(pd_obj):
     usage_mb = usage_b / 1024 ** 2 # convert bytes to megabytes
     return "{:03.3f} MB".format(usage_mb)
 
+def fix_zipcode(s):
+    ''' fix zip codes: '43212.0' --> '43212', '-1.0' --> np.nan, nan --> np.nan '''
+    fixed = str(s).split('.')[0]
+    if len(fixed) == 5:
+        return fixed
+    else:
+        return np.nan
+
+def fix_hour(pd_series):
+    '''fix hour column: 'HH:00' (string) --> HH (int8)'''
+    def fix_string(s):
+        '''drops the : and trailing zeroes ('22:00' --> '22')'''
+        return str(s).split(':')[0]
+    reduced =  pd_series.apply(fix_string)
+    return reduced.astype('int8')
+
+def unique_counter(pd_series):
+    '''reports the number (and percentage) of unique elements in a pd.Series'''
+    name = pd_series.name
+    u_len = len(pd_series.unique())
+    pd_len = len(pd_series)
+    print( '{0} has {1} unique elements ({2}%)'.format(name, u_len, 100*u_len/pd_len) )
+    return
+
 def load_data(data_dir=r'C:\PythonBC\Project\Data', fname='2019-04-27.csv', nrows='All', Verbose=False):
     '''loads a CSV or ZIP file to a pandas dataframe. note: can only work with a single compressed file, not a bundle of compressed files'''
-    def fix_zipcode(s):
-        ''' fix zip codes: '43212.0' --> '43212', '-1.0' --> '-1', nan --> 'nan' '''
-        return str(s).split('.')[0]
-    
-    def fix_hour(pd_series):
-        '''fix hour column: 'HH:00' (string) --> HH (int8)'''
-        def fix_string(s):
-            '''drops the : and trailing zeroes ('22:00' --> '22')'''
-            return str(s).split(':')[0]
-        reduced =  pd_series.apply(fix_string)
-        return reduced.astype('int8')
-    
-    def unique_counter(pd_series):
-        '''reports the number (and percentage) of unique elements in a pd.Series'''
-        name = pd_series.name
-        u_len = len(pd_series.unique())
-        pd_len = len(pd_series)
-        print( '{0} has {1} unique elements ({2}%)'.format(name, u_len, 100*u_len/pd_len) )
-        return
-    
+
     mycols = ['',
           'auction_id',
           'inventory_source',
@@ -75,7 +78,7 @@ def load_data(data_dir=r'C:\PythonBC\Project\Data', fname='2019-04-27.csv', nrow
     mydtype = {'auction_id': str,
         'inventory_source': 'category',
         'app_bundle': 'category',
-        'category': str,
+        'category': 'category',
         'inventory_interstitial': np.bool,
         'geo_zip': str,
         'platform_bandwidth': 'category',
@@ -101,15 +104,15 @@ def load_data(data_dir=r'C:\PythonBC\Project\Data', fname='2019-04-27.csv', nrow
     
     #load data
     if nrows == 'All':
-        df = pd.read_csv(data_dir + '\\' + fname, usecols=mycols[1:], dtype=mydtype)
+        df = pd.read_csv(os.path.join(data_dir, fname), usecols=mycols[1:], dtype=mydtype)
     elif int(nrows)>0: 
-        df = pd.read_csv(data_dir + '\\' + fname, usecols=mycols[1:], dtype=mydtype, nrows=int(nrows))
+        df = pd.read_csv(os.path.join(data_dir, fname), usecols=mycols[1:], dtype=mydtype, nrows=int(nrows))
     
     if Verbose:
         print('memory size after loading:', mem_usage(df))
 
     # do some dtype conversions on columns to save memory, clean data
-    df.bid_timestamp_utc = pd.to_datetime( df.bid_timestamp_utc, format='%Y-%m-%d %H:%M:%S.%f', errors='coerce' ) # fix timestamp
+    df.bid_timestamp_utc = pd.to_datetime( df.bid_timestamp_utc, format='%Y-%m-%d %H:%M:%S.%f', errors='coerce', utc=True ) # fix timestamp
     df.geo_zip = df.geo_zip.apply(fix_zipcode)
     df.hour = fix_hour(df.hour)
     df.geo_zip = df.geo_zip.astype('category')
@@ -117,28 +120,50 @@ def load_data(data_dir=r'C:\PythonBC\Project\Data', fname='2019-04-27.csv', nrow
     # drop useless columns
     df = df.drop(columns = ['auction_id']) # has no predictive power
     df = df.drop(columns = ['platform_os']) # only consists of 'Android' or '-1'
-    #df = df.drop(columns = ['day', 'month', 'year', 'hour']) #redundant with bid_timestamp_utc
+    df = df.drop(columns = ['day', 'month', 'year', 'hour']) #these numbers correspond to UTC, not useful
     if Verbose:
         print('memory size after downcasting:', mem_usage(df))
     return df
 
+def load_all_files(data_dir, type='csv'):
+    '''load all CSVs or ZIPs in data_dir into a single pd.dataframe'''
+    if type == 'csv':
+        all_files = [os.path.basename(x) for x in glob.glob(os.path.join(data_dir, '*.csv'))]
+    elif type == 'zip':
+        all_files = [os.path.basename(x) for x in glob.glob(os.path.join(data_dir, '*.zip'))]
+    df_from_each_file = (load_data(data_dir=data_dir, fname=f, nrows='All', Verbose=False) for f in all_files)
+    df = pd.concat(df_from_each_file, ignore_index=True)
+    return df
+
+def temp_save(df, fname='df.gzip'):
+    '''saves a pd.df to disk for later use using Parquet'''
+    df.to_parquet(fname, engine='pyarrow')
+    
+def temp_load(fname='df.gzip'):
+    '''loads a Parquet file that was saved using temp_save'''
+    _table = (pq.ParquetFile(fname)
+            .read(use_pandas_metadata=True))
+    df = _table.to_pandas(strings_to_categorical=True) # force strings to categoricals to save memory
+    return df
 #%% example usage
+'''
 fname = '2019-04-27.csv'
 fname_zip = '2019-04-27.zip'
 data_dir = r'C:\PythonBC\Project\Data'
 
 start = time.time()
 df = load_data(fname=fname, data_dir=data_dir, Verbose=True)
+#df = load_all_files(data_dir)
 end = time.time()
 print('time to load file: {0:3.3f} seconds'.format(end-start))
 
 # get an idea of how much memory we are using
 print(df.memory_usage(deep=True))
-print('size in memory:', mem_usage(df)) #311.583 MB
+print('size in memory:', mem_usage(df)) #173.278 MB
 
 # compare to default settings (raises an error)
 #df2 = pd.read_csv(data_dir + '\\' + fname)
 #print('size in memory:', mem_usage(df2)) # 1478 MB
-
+'''
 #%% try to plot things
 #df['platform_bandwidth'].value_counts().plot(kind='bar')
---
+++ zip_codes.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 20:29:35 2019
+
+@author: Greg Smith
+"""
+
+import pandas as pd
+import os
+import numpy as np
+
+class ZC():
+    ''' loads zip code information '''
+    def __init__(self, fdir=r'C:\PythonBC\Project', fname='zip_code_database.csv'):
+        self.fdir = fdir
+        self.fname = fname
+        self.ZC = self.init_zipcodes()
+        
+    def init_zipcodes(self):
+        ''' creates a pandas dataframe of zip codes, states, and time zones
+        data source: https://www.unitedstateszipcodes.org/zip-code-database/
+        '''
+        fdir = self.fdir
+        fname = self.fname
+        mycols = ['zipcode',
+                  'type',
+                  'decommissioned',
+                  'primary_city',
+                  'acceptable_cities',
+                  'unacceptable_cities',
+                  'state',
+                  'county',
+                  'timezone',
+                  'area_codes',
+                  'world_region',
+                  'country',
+                  'latitude',
+                  'longitude',
+                  'irs_estimated_population_2015']
+        mydtypes = {'zipcode': str,
+                  'type': 'category',
+                  'decommissioned': np.bool,
+                  'primary_city': str,
+                  'acceptable_cities': str,
+                  'unacceptable_cities': str,
+                  'state': str,
+                  'county': str,
+                  'timezone': str,
+                  'area_codes': 'category',
+                  'world_region': str,
+                  'country': str,
+                  'latitude': float,
+                  'longitude': float,
+                  'irs_estimated_population_2015': float}
+        ZC = pd.read_csv( os.path.join(fdir, fname), skiprows=1, names=mycols, dtype=mydtypes )
+        ZC = ZC.set_index('zipcode')
+        return ZC[['state', 'timezone', 'county']]
+        
+    def zip_to_tz(self, myzip):
+        ''' given a zip code, returns a time zone '''
+        ZC = self.ZC
+        try:
+            ans = ZC.timezone[ZC.index == myzip].values[0]
+        except:
+            ans = None
+        if type(ans) == float:
+            ans = None # empty values in database return nan (float)
+        return ans
+    
+    def zip_to_state(self, myzip):
+        ''' given a zip code, returns the US State '''
+        ZC = self.ZC
+        try:
+            ans = ZC.state[ZC.index == myzip].values[0]
+        except:
+            ans = None
+        if type(ans) == float:
+            ans = None # empty values in database return nan (float)
+        return ans
+
+    def zip_to_county(self, myzip):
+        ''' given a zip code, returns the county '''
+        ZC = self.ZC
+        try:
+            ans = ZC.county[ZC.index == myzip].values[0].split(' County')[0]
+        except:
+            ans = None
+        if type(ans) == float:
+            ans = None # empty values in database return nan (float)
+        return ans
+    
+    def zip_to_tz_2(self, myzip):
+        '''given a zip code, returns the time zone
+        this function is a vectorized version of zip_to_tz, about 600x faster'''
+        ZC = self.ZC
+        return ZC.reindex(myzip).timezone.values
+
+    def zip_to_state_2(self, myzip):
+        '''given a zip code, returns the state
+        this function is a vectorized version of zip_to_state, about 600x faster'''
+        ZC = self.ZC
+        return ZC.reindex(myzip).state.values
+    
+    def zip_to_county_2(self, myzip):
+        '''given a zip code, returns the county
+        this function is a vectorized version of zip_to_county, about 600x faster'''
+        ZC = self.ZC
+        return ZC.reindex(myzip).county.values
+    
+    def shift_tz_3(self, pd_df):
+        '''
+        shifts from UTC to local timezone
+        faster than .apply() but still kinda slow (0.5s for 50k rows)
+        '''
+        return pd_df['bid_timestamp_utc'].groupby(pd_df.tz).apply(lambda g: g.dt.tz_convert(g.name))
+    
+    def strip_tz(self, pd_s):
+        ''' strips the timezone information from a panda series.
+        SLOW! 1 second for 50k rows
+        '''
+        return pd_s.tz_localize(None)
+    
+    def shift_tz_wrap(self, pd_df):
+        '''wrapper function for strip_tz and shift_tz_3'''
+        pd_df['bid_timestamp_local'] = self.shift_tz_3(pd_df)
+        pd_df['bid_timestamp_local'] = pd_df['bid_timestamp_local'].apply( self.strip_tz )
+        return pd_df
+    
+    def local_hour(self, pd_df):
+        '''computes the hour of the local time'''
+        return pd_df["bid_timestamp_local"].dt.hour.values.astype('int8')
+
+#%% useage examples
+
+### single-line use
+#zc = ZC() #initialize
+#zc.zip_to_tz('43212') # 'America/New_York'
+#zc.zip_to_state('43212') # 'OH'
+#zc.zip_to_county('43212') # 'Frankin'
+
+# vectorized versions require a list or pd.series
+#zc.zip_to_tz_2(['43212']) # 'America/New_York'
+#zc.zip_to_state_2(['43212']) # 'OH'
+#zc.zip_to_county_2(['43212']) # 'Frankin'
+
+
+### using with pandas:
+# load data
+#df = load_data(fname=fname, data_dir=data_dir, nrows=50000)
+
+# add tz and state columns
+#df['tz'] = zc.zip_to_tz_2(df.geo_zip)
+#df['state'] = zc.zip_to_state_2(df.geo_zip)
+#df['tz'] = df.tz.astype('category')
+#df['state'] = df.state.astype('category')
+
+# compute local time
+#df = zc.shift_tz_wrap(df)
+#df['hour'] = zc.local_hour(df)
